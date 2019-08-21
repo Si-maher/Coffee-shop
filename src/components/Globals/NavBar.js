1ea6c62379990d1663a916c2a2e908b58fab192b
Simon Maher <simon.maher@integrify.io>
2019-08-21 09:21:24 +0300

    created navbar function to display/hide links

--- src/components/Globals/NavBar.js
+++ src/components/Globals/NavBar.js
@@ -1,25 +1,67 @@
 import React, { Component } from "react"
-import {Link} from 'gatsby'
-import logo from '../../images/logo.svg'
-import {FaCartArrowDown} from "react-icons/fa"
+import { Link } from "gatsby"
+import logo from "../../images/logo.svg"
+import { FaCartArrowDown } from "react-icons/fa"
+
 export default class NavBar extends Component {
-  state ={
-    navbarOpen :false,
-    css:'collapse navbar-collapse',
+  state = {
+    navbarOpen: false,
+    css: "collapse navbar-collapse",
     links: [
       {
-      id:1,
-      path:'/',
-      text:'home'
-    },
+        id: 1,
+        path: "/",
+        text: "home",
+      },
       {
-      id:2,
-      path:'/about',
-      text:'about'
-    },
-  ]
+        id: 2,
+        path: "/about",
+        text: "about",
+      },
+    ],
+  }
+  navBarHandler = () => {
+    this.state.navbarOpen
+      ? this.setState({ navbarOpen: false, css: "collapse navbar-collapse" })
+      : this.setState({
+          navbarOpen: true,
+          css: "collapse navbar-collapse show",
+        })
   }
   render() {
-    return <div>navbar content</div>
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="logo" />
+          {/* https://www.iconfinder.com/icons/185113/coffee_streamline_icon
+          Creative Commons (Attribution 3.0 Unported);
+          https://www.iconfinder.com/webalys  */}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navBarHandler}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li key={link.id} className="nav-item">
+                  <Link
+                    to={link.path}
+                    className="nav-link
+            text-capitalize"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </nav>
+    )
   }
 }
