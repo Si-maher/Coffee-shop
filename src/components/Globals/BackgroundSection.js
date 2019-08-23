cbcc73896d50e8ff4321980c66e0d140a51ec9b7
Simon Maher <simon.maher@integrify.io>
2019-08-23 09:50:14 +0300

    Added styles to the heroImage, props for the heroImage component

--- src/components/Globals/BackgroundSection.js
+++ src/components/Globals/BackgroundSection.js
@@ -1,6 +1,6 @@
 import React from "react"
 import HeroImage from "gatsby-background-image"
-
+// Background section takes four properties
 export default function BackgroundSection({
   img,
   styleClass,
@@ -16,6 +16,7 @@ export default function BackgroundSection({
     </HeroImage>
   )
 }
+// If no title is given in the index js, then this is the default setting
 BackgroundSection.defaultProps = {
   title: "default title",
   styleClass: "default-background",
