ea7bceb9bd4661cfebacddbf659fdc118bc5fb6a
Simon Maher <simon.maher@integrify.io>
2019-08-24 16:00:08 +0300

    CReated Footer component,folder. Added text and styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,13 +11,15 @@ import { useStaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css"
 import "./layout.css"
 import NavBar from "./Globals/NavBar"
+import Footer from "./Globals/Footer"
 
-const Layout = ({ children }) => 
+const Layout = ({ children }) => (
   <>
     <NavBar />
     {children}
+    <Footer />
   </>
-
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
