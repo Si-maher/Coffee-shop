ff1e9971c0507cf9c10b2bd88100ac349d78e962
Simon Maher <simon.maher@integrify.io>
2019-10-04 11:59:35 +0300

    created README, added live demo link

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Anna & Emma Kahvi Roastery`,
+    title: `Si and Hanna's coffee retreat`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
